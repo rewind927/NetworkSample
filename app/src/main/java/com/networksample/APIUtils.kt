0530c593b14fee0e5860ecca0b72490c0ecbbd0d
rewind927 <rewind927@gmail.com>
2020-08-29 23:03:19 +0800

    Revise PRIVATE_API_URL

--- app/src/main/java/com/networksample/APIUtils.kt
+++ app/src/main/java/com/networksample/APIUtils.kt
@@ -3,6 +3,6 @@ package com.networksample
 class APIUtils {
     companion object {
         const val PUBLIC_API_URL = "https://code-test.migoinc-dev.com/status"
-        const val PRIVATE_API_URL = "https://demo2285607.mockable.io/status"
+        const val PRIVATE_API_URL = "http://192.168.2.2/status"
     }
 }
